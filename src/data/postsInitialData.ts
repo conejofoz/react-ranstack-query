698dc7f611172c72dc5686d855a422e274ebb021
conejofoz <conejofoz@gmail.com>
2025-01-04 18:52:05 -0300

    dados iniciais e placeholder de query

---
+++ src/data/postsInitialData.ts
@@ -0,0 +1,22 @@
+import { Post } from "../types/Post"
+
+export const postsInitialData:Post[] = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "Post fake 1",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "Post fake 2",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "Post fake 3",
+        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    }
+]
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,5 +1,6 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query"
 import { getPost, getPosts } from "./api";
+import { postsInitialData } from "../data/postsInitialData";
 
 export const usePosts = (enabled:boolean, limit: number, start: number)=>{
     const query = useQuery({
@@ -8,7 +9,11 @@ export const usePosts = (enabled:boolean, limit: number, start: number)=>{
         queryFn: ()=> getPosts(limit, start),
         enabled: enabled,
         placeholderData: keepPreviousData, //mantem os dados anteriores até a chegada da nova requisição
+        initialData: postsInitialData
     });
+    /* Obs: initialData mantém os dados iniciais na query, placeholderData no lugar de initialData com
+    os dados iniciais funciona só como um placeholder mesmo.
+     */
     return query;
 }
 
