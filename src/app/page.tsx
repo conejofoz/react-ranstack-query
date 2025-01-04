30cc68e1f9d6179fa42ba2d437b23a7fcd48134c
conejofoz <conejofoz@gmail.com>
2025-01-04 10:55:33 -0300

    organizando as queries do tanstack em um arquivo separado para não ficarem na página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,23 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Post } from "../types/Post";
+import { getPosts } from "../utils/api";
+import { usePost, usePosts } from "../utils/queries";
 
 const Page = ()=>{
 
-  const query = useQuery({
-    queryKey: ['posts'],
-    queryFn: async (): Promise<Post[]> =>{
-      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      return result.data;
-    }
-  });
+  const posts = usePosts();
+  const post = usePost(10);
 
 
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl">Opa, tudo bem?</h1>
       {/* O controle de loading é feito pelo próprio tanstack */}
-      {query.isLoading && "Carregando..."}
+      {posts.isLoading && "Carregando..."}
 
       <ul>
-        {query.data?.map((item)=>(
+        {posts.data?.map((item)=>(
           <li key={item.id}>{item.title}</li>
         ))}
       </ul>
