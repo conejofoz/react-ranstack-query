152b076dc88f5cb7e6011e9a6f425d6eed4f4fa8
conejofoz <conejofoz@gmail.com>
2025-01-04 10:08:38 -0300

    adicionando o tanstack devtools

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react"
 
 type Props = {
@@ -11,6 +12,7 @@ export const Providers = ({children}: Props)=>{
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false} position="bottom" />
         </QueryClientProvider>
     )
 }
