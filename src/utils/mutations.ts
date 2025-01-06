808724ab9fe08bc53401bb17d8b21d023ed1e177
conejofoz <conejofoz@gmail.com>
2025-01-05 22:42:10 -0300

    atualizando query pelo mutation

--- src/utils/mutations.ts
+++ src/utils/mutations.ts
@@ -1,15 +1,23 @@
 import { useMutation } from "@tanstack/react-query"
 import { addPost } from "./api"
 import { queryClient } from "./queryClient"
+import { Post } from "../types/Post"
 
 export const useAddPost = ()=>{
     const mutation = useMutation({
         mutationFn: addPost,
-        onSuccess:()=>{
-            /* invalidar a mutation para que a lista de posts seja atualizada após ser inserido um novo */
-            queryClient.invalidateQueries({
-                queryKey:['posts'],
-            })
+        onSuccess:(newPost:Post)=>{
+            /* 
+            Adicionando o novo post retornado à lista de posts sem ter que fazer uma nova requisição
+            Não é muito recomendado pq a chance ocasionar algum efeito colateral é grande, ex: se a lista
+            estava ordenada e o novo post for adicionado no início ou no fim já temos um problema...
+             */
+            const posts = queryClient.getQueryData(['posts']) as Post[];
+
+            queryClient.setQueryData(['posts'], [newPost, ...posts]);
+        },
+        onError:(error)=>{
+            console.log(error)
         }
     })
     return mutation;
